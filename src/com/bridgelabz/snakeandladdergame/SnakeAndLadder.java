a3391678eec61781d47091eee5e6c42890a66139
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 12:39:15 +0530

    initial commit

---
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakeandladdergame;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder Game");
+    }
+}
