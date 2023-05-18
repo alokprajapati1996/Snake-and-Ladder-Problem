320c7d7bb09520f4c7e2845b4697b64bf4c37338
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 14:24:35 +0530

    Added:code random number one to six

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
         int startPositionPlayer=0;
+        int dice=(int) (Math.random()*6)+1;
+        System.out.println(dice);
     }
 }
