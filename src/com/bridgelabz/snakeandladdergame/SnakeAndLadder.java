c91765ade6bd5f61ce042211c3e0301946fcbb09
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-18 13:12:39 +0530

    Added:code starting position of player

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -3,5 +3,6 @@ package com.bridgelabz.snakeandladdergame;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Snake and Ladder Game");
+        int startPositionPlayer=0;
     }
 }
