a6d67710a27b8d2eddd97b66753caa31e652447f
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-23 10:02:24 +0530

    Added:code repeat till the Player reaches the winning

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -1,30 +1,35 @@
 package com.bridgelabz.snakeandladdergame;
 public class SnakeAndLadder {
-        public static void main(String[] args) {
-            System.out.println("Snake and Ladder Game");
-            int player = 0;
-            final int SNAKE = 2;
-            final int LADDER = 1;
-            final int STAY_SAME_POSITION = 0;
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder Game");
+        int player = 0;
+        final int SNAKE = 2;
+        final int LADDER = 1;
+        final int STAY_SAME_POSITION = 0;
+        while (player <= 99) {
             int dice = (int) (Math.random() * 6) + 1;
-
             int checkPlayOption = (int) (Math.random() * 3);
+
             switch (checkPlayOption) {
                 case STAY_SAME_POSITION:
                     player = player;
                     break;
                 case LADDER:
-
                     player = player + dice;
+
                     break;
                 case SNAKE:
                     player = player - dice;
-
+                    if (player < 0) {
+                        player = 0;
+                    }
                     break;
             }
 
         }
+    }
 }
+
 
 
 
