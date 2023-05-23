650443203f2a1ead44f68cd7710f3d276c0428a1
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-23 09:38:19 +0530

    Added:code check position for play player

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -1,10 +1,101 @@
 package com.bridgelabz.snakeandladdergame;
-
 public class SnakeAndLadder {
-    public static void main(String[] args) {
-        System.out.println("Snake and Ladder Game");
-        int startPositionPlayer=0;
-        int dice=(int) (Math.random()*6)+1;
-        System.out.println(dice);
-    }
+        public static void main(String[] args) {
+            System.out.println("Snake and Ladder Game");
+            int player = 0;
+            final int SNAKE = 2;
+            final int LADDER = 1;
+            final int STAY_SAME_POSITION = 0;
+            int dice = (int) (Math.random() * 6) + 1;
+
+            int checkPlayOption = (int) (Math.random() * 3);
+            switch (checkPlayOption) {
+                case STAY_SAME_POSITION:
+                    player = player;
+                    break;
+                case LADDER:
+
+                    player = player + dice;
+                    break;
+                case SNAKE:
+                    player = player - dice;
+
+                    break;
+            }
+
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
